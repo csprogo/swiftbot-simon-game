6d12b275db2df985cd4edb310e6fa3512ed28c5e
csprogo <2409644@brunel.ac.uk>
2024-11-21 06:11:44 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,33 @@
 // imports
 import swiftbot.*; // imports swiftbot library from the JAR file
+import java.util.Scanner; // imports java ability to take user input
+
+
+// current references used 1. Java Does my swiftbot work
+
 
 public class Main {
     static SwiftBotAPI swiftBot;
-    public static void main(String[] args) {
-        {
-            // this creates a new instance of the API and assigns it to swiftBot variable
+    public static void main(String[] args) throws InterruptedException {
+        try {
             swiftBot = new SwiftBotAPI();
+        } catch (Exception e) {
+            /*
+             * Outputs a warning if I2C is disabled. This only needs to be turned on once,
+             * so you won't need to worry about this problem again!
+             */
+            System.out.println("\nI2C disabled!");
+            System.out.println("Run the following command:");
+            System.out.println("sudo raspi-config nonint do_i2c 0\n");
+            System.exit(5);
         }
+        // creates menu for user interaction
+        System.out.println("\n*****************************************************************");
+        System.out.println("*****************************************************************");
+        System.out.println("");
+        System.out.println("\t\t\tWelcome to the Simon Game!");
+        System.out.println("");
+        System.out.println("*****************************************************************");
+        System.out.println("*****************************************************************\n");
     }
 }
