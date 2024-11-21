e8356aa2c47c47155a2da7db568cc8301df9310f
csprogo <2409644@brunel.ac.uk>
2024-11-21 05:59:44 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 // imports
-
-import swiftbot.*
+import swiftbot.*; // imports swiftbot library from the JAR file
 
 public class Main {
+    static SwiftBotAPI swiftBot;
     public static void main(String[] args) {
-        System.out.println()
+        {
+            // this creates a new instance of the API and assigns it to swiftBot variable
+            swiftBot = new SwiftBotAPI();
+        }
     }
 }
